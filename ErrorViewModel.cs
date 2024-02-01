64fbf010da530c152a5c6165faedfce53b874c08
Angel Acuna <AngelAcuna2000@gmail.com>
2024-02-01 13:02:02 -0500

    Update

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Models.ErrorViewModel
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
