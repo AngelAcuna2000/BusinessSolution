d037c5630e6dd78aa8129462499b74e48bf11787
Angel Acuna <AngelAcuna2000@gmail.com>
2024-02-07 13:48:41 -0500

    Updated Comments

--- InquiryApp/Controllers/InquiryController.cs
+++ InquiryApp/Controllers/InquiryController.cs
@@ -14,12 +14,16 @@ namespace InquiryApp.Controllers
             _repo = repo;
         }
 
+        // Display table listing all inquiries and their details in the database
         public IActionResult Index() => View(_repo.GetAllInquiries());
 
+        // Display details of a specific inquiry
         public IActionResult ViewInquiry(int id) => View(_repo.GetInquiry(id));
 
+        // Display form for updating an inquiry
         public IActionResult UpdateInquiry(int id) => View(_repo.GetInquiry(id));
 
+        // Update inquiry in the database and display the updated details
         [HttpPost]
         public IActionResult UpdateInquiryToDatabase(InquiryModel inquiry)
         {
@@ -28,6 +32,7 @@ namespace InquiryApp.Controllers
             return RedirectToAction(nameof(ViewInquiry), new { id = inquiry.Inquiry_ID });
         }
 
+        // Delete selected inquiry and display the updated table with all remaining inquiries
         [HttpPost]
         public IActionResult DeleteInquiry(InquiryModel inquiry)
         {
