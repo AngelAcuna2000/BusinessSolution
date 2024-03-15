01d43d4332cd2f7f2b74096ff47c95d7bdb72524
Angel Acuna <AngelAcuna2000@gmail.com>
2024-03-15 09:32:03 -0400

    BusinessSolution

--- BusinessWebsite/Program.cs
+++ BusinessWebsite/Program.cs
@@ -1,33 +1,23 @@
-using BusinessSolutionShared;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.SetBasePath(AppContext.BaseDirectory)
-                     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                     .AddEnvironmentVariables();
+builder.Configuration.SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
-builder.Services.AddScoped<MySqlConnection>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-
-    var connectionString = configuration.GetConnectionString("client_inquiries");
-
-    return new MySqlConnection(connectionString);
-});
-
-builder.Services.AddScoped<DbConnector>();
-
 builder.Services.AddScoped<IDbConnection>(provider =>
 {
-    var dbConnector = provider.GetRequiredService<DbConnector>();
+    var connectionString = builder.Configuration.GetConnectionString("client_inquiries");
 
-    return dbConnector.CreateConnection();
+    var connection = new MySqlConnection(connectionString);
+
+    connection.Open();
+
+    return connection;
 });
 
 var app = builder.Build();
