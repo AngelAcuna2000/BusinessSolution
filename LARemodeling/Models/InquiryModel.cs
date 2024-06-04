08cdce5980c34da90ab18349ee91f7caa73e8dd0
Angel Acuna <AngelAcuna2000@gmail.com>
2024-06-03 20:12:14 -0400

    LARemodeling

--- LARemodeling/Models/InquiryModel.cs
+++ LARemodeling/Models/InquiryModel.cs
@@ -3,7 +3,7 @@
 public class InquiryModel
 {
     public int Inquiry_ID { get; set; }
-    public DateTime Date { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
     public string Name { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
